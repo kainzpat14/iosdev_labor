a4a3e31ee051f5bc7ea05ba1e0b66f37768f86c6
patrickkainz <patrick.kainz@edu.fh-joanneum.at>
2018-10-19 14:37:10 +0200

    interesiert mi net

---
+++ lab191018/lab191018/lab191018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab191018
+//
+//  Created by Patrick Kainz on 19.10.18.
+//  Copyright © 2018 Patrick Kainz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
