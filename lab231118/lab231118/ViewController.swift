dd345b69cd5f2078a086f0a37c195e2a47cea9c9
patrickkainz <patrick.kainz@edu.fh-joanneum.at>
2018-11-23 19:11:32 +0100

    ads

--- lab231118/lab231118/ViewController.swift
+++ lab231118/lab231118/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        map.delegate = self
+        map.showsUserLocation = true
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        }else{
+            locationManager.requestWhenInUseAuthorization()
+        }
     }
-
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView .setCenter(userLocation.coordinate, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        let coordinateRegion = MKCoordinateRegion(center: newLocation.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+        map.setRegion(map.regionThatFits(coordinateRegion), animated: true)
+    }
 
 }
 
