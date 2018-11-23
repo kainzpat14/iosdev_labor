39837671ce0863bce0356f12f714e8566b085646
patrickkainz <patrick.kainz@edu.fh-joanneum.at>
2018-11-23 18:49:45 +0100

    added new

---
+++ lab231118/lab231118/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab231118
+//
+//  Created by Patrick Kainz on 23.11.18.
+//  Copyright © 2018 Patrick Kainz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ lab231118/lab231118Tests/lab231118Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lab231118Tests.swift
+//  lab231118Tests
+//
+//  Created by Patrick Kainz on 23.11.18.
+//  Copyright © 2018 Patrick Kainz. All rights reserved.
+//
+
+import XCTest
+@testable import lab231118
+
+class lab231118Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
