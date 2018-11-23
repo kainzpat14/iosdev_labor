1d332bc183812df8f50e9be5766118cd5c6a5f6b
patrickkainz <patrick.kainz@edu.fh-joanneum.at>
2018-11-23 19:52:40 +0100

    a

--- lab231118/lab231118/ViewController.swift
+++ lab231118/lab231118/ViewController.swift
@@ -11,29 +11,49 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     let locationManager = CLLocationManager()
+    let annotation = MKPointAnnotation()
+    
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+        
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 47.453990, longitude: 15.331747)
+        map.addAnnotation(annotation)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         map.delegate = self
-        map.showsUserLocation = true
+        
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
             locationManager.startUpdatingLocation()
-            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+            map.showsUserLocation = true
         }else{
             locationManager.requestWhenInUseAuthorization()
         }
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        mapView .setCenter(userLocation.coordinate, animated: true)
+        //stolen from https://stackoverflow.com/questions/14760582/mkmapview-zoom-user-location-and-annotation
+        let userPos = mapView.userLocation.coordinate
+        let annotationPos = annotation.coordinate
+        let userPoint = MKMapPoint(userPos);
+        let annotationPoint = MKMapPoint(annotationPos)
+        let userRect = MKMapRect(x: userPoint.x,y: userPoint.y,width: 0,height: 0)
+        let annotationRect = MKMapRect(x: annotationPoint.x,y: annotationPoint.y,width: 0,height: 0)
+        let unionRect = userRect.union(annotationRect)
+        let fittingRect = mapView.mapRectThatFits(unionRect, edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40))
+        mapView.setVisibleMapRect(fittingRect, animated: true)
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let newLocation = locations.last!
-        let coordinateRegion = MKCoordinateRegion(center: newLocation.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
-        map.setRegion(map.regionThatFits(coordinateRegion), animated: true)
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else { print("not enabled"); return }
+        map.showsUserLocation = true
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
 }
 
